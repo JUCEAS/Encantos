fb4c37f5b547e0350cbb9026a6b97a45454f8ea7
JUCEAS <juceas19@gmail.com>
2026-09-21 22:16:49 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker de Encantos — habilita el uso 100% sin internet
-const CACHE_NAME = 'encantos-cache-v3';
+const CACHE_NAME = 'encantos-cache-v4';
 const ASSETS = [
   './',
   './index.html',
